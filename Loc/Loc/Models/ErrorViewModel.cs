2abc1dd9636e5a01b62984595f7e12fe0a605223
Administrator <Administrator@PMT2011>
2026-06-19 15:25:45 +0700

    text

---
+++ Loc/Loc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Loc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
